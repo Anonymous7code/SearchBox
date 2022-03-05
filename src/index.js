0cece80291ef31acc2a5200670f98ff65fe2a4f5
Sajal Tiwari <sajaltiwari003@gmail.com>
2022-03-05 13:49:40 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./global.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ResultContextProvider } from "./contexts/ResultContextProvider";
+
+ReactDOM.render(
+  <ResultContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ResultContextProvider>,
+  document.getElementById("root")
+);
