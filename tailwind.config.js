0a4834a46764ae60ef2e80e3b15820823f7cb64c
Sajal Tiwari <sajaltiwari003@gmail.com>
2022-03-05 13:48:21 +0530

    Added Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,md,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
