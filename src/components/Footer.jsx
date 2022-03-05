2d40b32a91d382f1b7ea061079bd612f480c313d
Sajal Tiwari <sajaltiwari003@gmail.com>
2022-03-05 13:49:53 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <>
+      <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+        <h1>
+          2022 SearchBox, OSS - {" "}@
+          <a
+            href="https://github.com/Anonymous7code"
+            className="underline text-green-600 dark:text-yellow-500"
+          >
+          Anonymous7code
+          </a>{" "}
+        </h1>
+      </div>
+    </>
+  );
+};
